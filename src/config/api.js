5c6eb8fe3ad3ff0bbc2812f59d3a2a9b0ffc6826
Basavarajakjbasuu <basavarajakj06@gmail.com>
2023-10-13 12:46:42 +0530

    Fixes irregular card component

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = "4557db3db3b45e65e88c647db5d30499";
+const API_KEY = "USE-YOUR-API-KEY";
 export const endpointPath = (country, category) =>
   `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
 export const endpointSearch = (searchQuery) =>
